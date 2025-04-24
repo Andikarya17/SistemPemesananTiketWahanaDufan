ae756a84543ae9188d88911447f7b491daef024c
Andikarya17 <andikaryaputra17@gmail.com>
2025-04-24 13:24:02 +0700

    update admin

--- SistemPemesananTiketWahanaDufan/admin.Designer.cs
+++ SistemPemesananTiketWahanaDufan/admin.Designer.cs
@@ -4,10 +4,9 @@
     {
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.ComboBox cmbAdmin;
+
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -17,144 +16,152 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.cmbAdmin = new System.Windows.Forms.ComboBox();
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblWahana = new System.Windows.Forms.Label();
+            this.lblPengunjung = new System.Windows.Forms.Label();
+            this.lblPesanan = new System.Windows.Forms.Label();
+
+            this.txtNamaWahana = new System.Windows.Forms.TextBox();
+            this.txtHargaTiket = new System.Windows.Forms.TextBox();
+            this.txtKapasitas = new System.Windows.Forms.TextBox();
+            this.cmbTipeTiket = new System.Windows.Forms.ComboBox();
+            this.btnTambahWahana = new System.Windows.Forms.Button();
+            this.btnUbahWahana = new System.Windows.Forms.Button();
+            this.btnHapusWahana = new System.Windows.Forms.Button();
+            this.dgvWahana = new System.Windows.Forms.DataGridView();
+
+            this.txtNamaPengunjung = new System.Windows.Forms.TextBox();
+            this.txtEmailPengunjung = new System.Windows.Forms.TextBox();
+            this.btnUbahPengunjung = new System.Windows.Forms.Button();
+            this.btnHapusPengunjung = new System.Windows.Forms.Button();
+            this.dgvPengunjung = new System.Windows.Forms.DataGridView();
+
             this.cmbPengunjung = new System.Windows.Forms.ComboBox();
             this.cmbWahanaPesanan = new System.Windows.Forms.ComboBox();
-            this.cmbTipeTiket = new System.Windows.Forms.ComboBox();
-            this.txtJumlahTiket = new System.Windows.Forms.TextBox();
             this.dtpTanggal = new System.Windows.Forms.DateTimePicker();
-            this.dgvPesanan = new System.Windows.Forms.DataGridView();
+            this.txtJumlahTiket = new System.Windows.Forms.TextBox();
+            this.cmbStatusPesanan = new System.Windows.Forms.ComboBox();
             this.btnTambahPesanan = new System.Windows.Forms.Button();
             this.btnHapusPesanan = new System.Windows.Forms.Button();
-            this.btnRefresh = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvPesanan)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // cmbAdmin
-            // 
+            this.btnUpdateStatus = new System.Windows.Forms.Button();
+            this.dgvPesanan = new System.Windows.Forms.DataGridView();
+
+            this.cmbAdmin = new System.Windows.Forms.ComboBox();
             this.cmbAdmin.FormattingEnabled = true;
-            this.cmbAdmin.Location = new System.Drawing.Point(20, 20);
+            this.cmbAdmin.Location = new System.Drawing.Point(150, 50); // Sesuaikan lokasi
             this.cmbAdmin.Name = "cmbAdmin";
-            this.cmbAdmin.Size = new System.Drawing.Size(200, 24);
-            this.cmbAdmin.TabIndex = 0;
-            this.cmbAdmin.Text = "Pilih Admin";
-            // 
-            // cmbPengunjung
-            // 
-            this.cmbPengunjung.FormattingEnabled = true;
-            this.cmbPengunjung.Location = new System.Drawing.Point(20, 60);
-            this.cmbPengunjung.Name = "cmbPengunjung";
-            this.cmbPengunjung.Size = new System.Drawing.Size(200, 24);
-            this.cmbPengunjung.TabIndex = 1;
-            this.cmbPengunjung.Text = "Pilih Pengunjung";
-            // 
-            // cmbWahanaPesanan
-            // 
-            this.cmbWahanaPesanan.FormattingEnabled = true;
-            this.cmbWahanaPesanan.Location = new System.Drawing.Point(20, 100);
-            this.cmbWahanaPesanan.Name = "cmbWahanaPesanan";
-            this.cmbWahanaPesanan.Size = new System.Drawing.Size(200, 24);
-            this.cmbWahanaPesanan.TabIndex = 2;
-            this.cmbWahanaPesanan.Text = "Pilih Wahana";
-            // 
-            // cmbTipeTiket
-            // 
-            this.cmbTipeTiket.FormattingEnabled = true;
-            this.cmbTipeTiket.Location = new System.Drawing.Point(20, 140);
-            this.cmbTipeTiket.Name = "cmbTipeTiket";
-            this.cmbTipeTiket.Size = new System.Drawing.Size(200, 24);
-            this.cmbTipeTiket.TabIndex = 3;
-            this.cmbTipeTiket.Text = "Pilih Tipe Tiket";
-            // 
-            // txtJumlahTiket
-            // 
-            this.txtJumlahTiket.Location = new System.Drawing.Point(20, 180);
-            this.txtJumlahTiket.Name = "txtJumlahTiket";
-            this.txtJumlahTiket.Size = new System.Drawing.Size(200, 22);
-            this.txtJumlahTiket.TabIndex = 4;
-            // 
-            // dtpTanggal
-            // 
-            this.dtpTanggal.Location = new System.Drawing.Point(20, 220);
-            this.dtpTanggal.Name = "dtpTanggal";
-            this.dtpTanggal.Size = new System.Drawing.Size(200, 22);
-            this.dtpTanggal.TabIndex = 5;
-            // 
-            // dgvPesanan
-            // 
-            this.dgvPesanan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvPesanan.Location = new System.Drawing.Point(250, 20);
-            this.dgvPesanan.Name = "dgvPesanan";
-            this.dgvPesanan.RowHeadersWidth = 51;
-            this.dgvPesanan.Size = new System.Drawing.Size(500, 300);
-            this.dgvPesanan.TabIndex = 6;
-            // 
-            // btnTambahPesanan
-            // 
-            this.btnTambahPesanan.Location = new System.Drawing.Point(20, 260);
-            this.btnTambahPesanan.Name = "btnTambahPesanan";
-            this.btnTambahPesanan.Size = new System.Drawing.Size(200, 30);
-            this.btnTambahPesanan.TabIndex = 7;
-            this.btnTambahPesanan.Text = "Tambah Pesanan";
-            this.btnTambahPesanan.UseVisualStyleBackColor = true;
-            // 
-            // btnHapusPesanan
-            // 
-            this.btnHapusPesanan.Location = new System.Drawing.Point(20, 300);
-            this.btnHapusPesanan.Name = "btnHapusPesanan";
-            this.btnHapusPesanan.Size = new System.Drawing.Size(200, 30);
-            this.btnHapusPesanan.TabIndex = 8;
-            this.btnHapusPesanan.Text = "Hapus Pesanan";
-            this.btnHapusPesanan.UseVisualStyleBackColor = true;
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.Location = new System.Drawing.Point(20, 340);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(200, 30);
-            this.btnRefresh.TabIndex = 9;
-            this.btnRefresh.Text = "Refresh";
-            this.btnRefresh.UseVisualStyleBackColor = true;
-            // 
-            // MainForm
-            // 
-            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.cmbAdmin.Size = new System.Drawing.Size(200, 21);
+            this.cmbAdmin.TabIndex = 5;
             this.Controls.Add(this.cmbAdmin);
-            this.Controls.Add(this.cmbPengunjung);
-            this.Controls.Add(this.cmbWahanaPesanan);
-            this.Controls.Add(this.cmbTipeTiket);
-            this.Controls.Add(this.txtJumlahTiket);
-            this.Controls.Add(this.dtpTanggal);
-            this.Controls.Add(this.dgvPesanan);
-            this.Controls.Add(this.btnTambahPesanan);
-            this.Controls.Add(this.btnHapusPesanan);
-            this.Controls.Add(this.btnRefresh);
-            this.Name = "MainForm";
-            this.Text = "Sistem Pemesanan Tiket Wahana Dufan";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvPesanan)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+
+
+            // Label Section
+            this.lblTitle.Text = "Admin Dashboard - Tiket Wahana Dufan";
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.Location = new System.Drawing.Point(250, 10);
+            this.lblTitle.Size = new System.Drawing.Size(400, 30);
+
+            this.lblWahana.Text = "Kelola Wahana";
+            this.lblWahana.Location = new System.Drawing.Point(20, 50);
+            this.lblWahana.Size = new System.Drawing.Size(150, 20);
+
+            this.lblPengunjung.Text = "Kelola Pengunjung";
+            this.lblPengunjung.Location = new System.Drawing.Point(20, 270);
+            this.lblPengunjung.Size = new System.Drawing.Size(150, 20);
+
+            this.lblPesanan.Text = "Kelola Pesanan";
+            this.lblPesanan.Location = new System.Drawing.Point(20, 490);
+            this.lblPesanan.Size = new System.Drawing.Size(150, 20);
+
+            // Wahana Section
+            this.txtNamaWahana.Location = new System.Drawing.Point(20, 80);
+            this.txtNamaWahana.Size = new System.Drawing.Size(150, 22);
+            this.txtHargaTiket.Location = new System.Drawing.Point(180, 80);
+            this.txtHargaTiket.Size = new System.Drawing.Size(100, 22);
+            this.txtKapasitas.Location = new System.Drawing.Point(290, 80);
+            this.txtKapasitas.Size = new System.Drawing.Size(80, 22);
+            this.cmbTipeTiket.Location = new System.Drawing.Point(380, 80);
+            this.cmbTipeTiket.Size = new System.Drawing.Size(120, 22);
+            this.btnTambahWahana.Location = new System.Drawing.Point(510, 78);
+            this.btnTambahWahana.Text = "Tambah";
+            this.btnUbahWahana.Location = new System.Drawing.Point(590, 78);
+            this.btnUbahWahana.Text = "Ubah";
+            this.btnHapusWahana.Location = new System.Drawing.Point(670, 78);
+            this.btnHapusWahana.Text = "Hapus";
+            this.dgvWahana.Location = new System.Drawing.Point(20, 110);
+            this.dgvWahana.Size = new System.Drawing.Size(760, 140);
+
+            // Pengunjung Section
+            this.txtNamaPengunjung.Location = new System.Drawing.Point(20, 300);
+            this.txtEmailPengunjung.Location = new System.Drawing.Point(180, 300);
+            this.btnUbahPengunjung.Location = new System.Drawing.Point(340, 298);
+            this.btnUbahPengunjung.Text = "Ubah";
+            this.btnHapusPengunjung.Location = new System.Drawing.Point(420, 298);
+            this.btnHapusPengunjung.Text = "Hapus";
+            this.dgvPengunjung.Location = new System.Drawing.Point(20, 330);
+            this.dgvPengunjung.Size = new System.Drawing.Size(760, 140);
+
+            // Pesanan Section
+            this.cmbPengunjung.Location = new System.Drawing.Point(20, 520);
+            this.cmbWahanaPesanan.Location = new System.Drawing.Point(180, 520);
+            this.dtpTanggal.Location = new System.Drawing.Point(340, 520);
+            this.txtJumlahTiket.Location = new System.Drawing.Point(500, 520);
+            this.cmbStatusPesanan.Location = new System.Drawing.Point(600, 520);
+            this.btnTambahPesanan.Location = new System.Drawing.Point(20, 550);
+            this.btnTambahPesanan.Text = "Tambah";
+            this.btnHapusPesanan.Location = new System.Drawing.Point(100, 550);
+            this.btnHapusPesanan.Text = "Hapus";
+            this.btnUpdateStatus.Location = new System.Drawing.Point(180, 550);
+            this.btnUpdateStatus.Text = "Update Status";
+            this.dgvPesanan.Location = new System.Drawing.Point(20, 580);
+            this.dgvPesanan.Size = new System.Drawing.Size(760, 140);
+
+            // Add Controls
+            this.Controls.AddRange(new System.Windows.Forms.Control[] {
+                lblTitle, lblWahana, lblPengunjung, lblPesanan,
+                txtNamaWahana, txtHargaTiket, txtKapasitas, cmbTipeTiket,
+                btnTambahWahana, btnUbahWahana, btnHapusWahana, dgvWahana,
+                txtNamaPengunjung, txtEmailPengunjung,
+                btnUbahPengunjung, btnHapusPengunjung, dgvPengunjung,
+                cmbPengunjung, cmbWahanaPesanan, dtpTanggal, txtJumlahTiket,
+                cmbStatusPesanan, btnTambahPesanan, btnHapusPesanan, btnUpdateStatus, dgvPesanan
+            });
+
+            this.Text = "Admin Dashboard";
+            this.ClientSize = new System.Drawing.Size(800, 750);
         }
 
-        #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblWahana;
+        private System.Windows.Forms.Label lblPengunjung;
+        private System.Windows.Forms.Label lblPesanan;
 
-        private System.Windows.Forms.ComboBox cmbAdmin;
+        private System.Windows.Forms.TextBox txtNamaWahana;
+        private System.Windows.Forms.TextBox txtHargaTiket;
+        private System.Windows.Forms.TextBox txtKapasitas;
+        private System.Windows.Forms.ComboBox cmbTipeTiket;
+        private System.Windows.Forms.Button btnTambahWahana;
+        private System.Windows.Forms.Button btnUbahWahana;
+        private System.Windows.Forms.Button btnHapusWahana;
+        private System.Windows.Forms.DataGridView dgvWahana;
+
+        private System.Windows.Forms.TextBox txtNamaPengunjung;
+        private System.Windows.Forms.TextBox txtEmailPengunjung;
+        private System.Windows.Forms.Button btnUbahPengunjung;
+        private System.Windows.Forms.Button btnHapusPengunjung;
+        private System.Windows.Forms.DataGridView dgvPengunjung;
+
         private System.Windows.Forms.ComboBox cmbPengunjung;
         private System.Windows.Forms.ComboBox cmbWahanaPesanan;
-        private System.Windows.Forms.ComboBox cmbTipeTiket;
-        private System.Windows.Forms.TextBox txtJumlahTiket;
         private System.Windows.Forms.DateTimePicker dtpTanggal;
-        private System.Windows.Forms.DataGridView dgvPesanan;
+        private System.Windows.Forms.TextBox txtJumlahTiket;
+        private System.Windows.Forms.ComboBox cmbStatusPesanan;
         private System.Windows.Forms.Button btnTambahPesanan;
         private System.Windows.Forms.Button btnHapusPesanan;
-        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnUpdateStatus;
+        private System.Windows.Forms.DataGridView dgvPesanan;
     }
 }
--- SistemPemesananTiketWahanaDufan/admin.cs
+++ SistemPemesananTiketWahanaDufan/admin.cs
@@ -7,7 +7,7 @@ namespace TiketWahanaApp
 {
     public partial class MainForm : Form
     {
-        private string connString = @"Data Source=Andikarya\ANDIKAARYA;Initial Catalog=TiketwahanDufan2;Integrated Security=True";
+        private string connString = @"Data Source=Andikarya\\ANDIKAARYA;Initial Catalog=TiketwahanDufan2;Integrated Security=True";
 
         public MainForm()
         {
@@ -16,132 +16,203 @@ namespace TiketWahanaApp
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            LoadComboAdmins();
-            LoadComboPengunjung();
-            LoadComboWahana();
+            LoadWahana();
+            LoadPengunjung();
             LoadPesanan();
+            LoadAdmin();
         }
 
-        #region — Load Comboboxes —
-        private void LoadComboAdmins()
+        #region — CRUD Wahana —
+        private void LoadWahana()
         {
-            SqlConnection conn = new SqlConnection(connString);
-            try
+            using (SqlConnection conn = new SqlConnection(connString))
             {
-                SqlDataAdapter da = new SqlDataAdapter("SELECT AdminID, Nama FROM Admin", conn);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Wahana", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvWahana.DataSource = dt;
+            }
+        }
+
+        private void btnTambahWahana_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Wahana (NamaWahana, TipeTiket, HargaTiket, KapasitasHarian, AdminID) VALUES (@nama, @tipe, @harga, @kapasitas, @adminID)", conn);
+                cmd.Parameters.AddWithValue("@nama", txtNamaWahana.Text);
+                cmd.Parameters.AddWithValue("@tipe", cmbTipeTiket.Text);
+                cmd.Parameters.AddWithValue("@harga", decimal.Parse(txtHargaTiket.Text));
+                cmd.Parameters.AddWithValue("@kapasitas", int.Parse(txtKapasitas.Text));
+                cmd.Parameters.AddWithValue("@adminID", (int)cmbAdmin.SelectedValue);
+                cmd.ExecuteNonQuery();
+                LoadWahana();
+            }
+        }
+
+        private void btnUbahWahana_Click(object sender, EventArgs e)
+        {
+            if (dgvWahana.SelectedRows.Count > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Wahana SET NamaWahana=@nama, TipeTiket=@tipe, HargaTiket=@harga, KapasitasHarian=@kapasitas WHERE WahanaID=@id", conn);
+                    cmd.Parameters.AddWithValue("@nama", txtNamaWahana.Text);
+                    cmd.Parameters.AddWithValue("@tipe", cmbTipeTiket.Text);
+                    cmd.Parameters.AddWithValue("@harga", decimal.Parse(txtHargaTiket.Text));
+                    cmd.Parameters.AddWithValue("@kapasitas", int.Parse(txtKapasitas.Text));
+                    cmd.Parameters.AddWithValue("@id", dgvWahana.SelectedRows[0].Cells["WahanaID"].Value);
+                    cmd.ExecuteNonQuery();
+                    LoadWahana();
+                }
+            }
+        }
+
+        private void btnHapusWahana_Click(object sender, EventArgs e)
+        {
+            if (dgvWahana.SelectedRows.Count > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Wahana WHERE WahanaID=@id", conn);
+                    cmd.Parameters.AddWithValue("@id", dgvWahana.SelectedRows[0].Cells["WahanaID"].Value);
+                    cmd.ExecuteNonQuery();
+                    LoadWahana();
+                }
+            }
+        }
+
+        private void LoadAdmin()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT AdminID, NamaAdmin FROM Admin", conn);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 cmbAdmin.DataSource = dt;
+                cmbAdmin.DisplayMember = "NamaAdmin";
                 cmbAdmin.ValueMember = "AdminID";
-                cmbAdmin.DisplayMember = "Nama";
-            }
-            finally
-            {
-                conn.Dispose();
-            }
-        }
-
-        private void LoadComboPengunjung()
-        {
-            SqlConnection conn = new SqlConnection(connString);
-            try
-            {
-                SqlDataAdapter da = new SqlDataAdapter("SELECT PengunjungID, Nama FROM Pengunjung", conn);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                cmbPengunjung.DataSource = dt;
-                cmbPengunjung.ValueMember = "PengunjungID";
-                cmbPengunjung.DisplayMember = "Nama";
-            }
-            finally
-            {
-                conn.Dispose();
-            }
-        }
-
-        private void LoadComboWahana()
-        {
-            SqlConnection conn = new SqlConnection(connString);
-            try
-            {
-                SqlDataAdapter da = new SqlDataAdapter("SELECT WahanaID, NamaWahana FROM Wahana", conn);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                cmbWahanaPesanan.DataSource = dt;
-                cmbWahanaPesanan.ValueMember = "WahanaID";
-                cmbWahanaPesanan.DisplayMember = "NamaWahana";
-            }
-            finally
-            {
-                conn.Dispose();
             }
         }
         #endregion
 
-        #region — CRUD Pesanan —
+        #region — CRUD Pengunjung —
+        private void LoadPengunjung()
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Pengunjung", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvPengunjung.DataSource = dt;
+            }
+        }
+
+        private void btnUbahPengunjung_Click(object sender, EventArgs e)
+        {
+            if (dgvPengunjung.SelectedRows.Count > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Pengunjung SET Nama=@nama, Email=@email WHERE PengunjungID=@id", conn);
+                    cmd.Parameters.AddWithValue("@nama", txtNamaPengunjung.Text);
+                    cmd.Parameters.AddWithValue("@email", txtEmailPengunjung.Text);
+                    cmd.Parameters.AddWithValue("@id", dgvPengunjung.SelectedRows[0].Cells["PengunjungID"].Value);
+                    cmd.ExecuteNonQuery();
+                    LoadPengunjung();
+                }
+            }
+        }
+
+        private void btnHapusPengunjung_Click(object sender, EventArgs e)
+        {
+            if (dgvPengunjung.SelectedRows.Count > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Pengunjung WHERE PengunjungID=@id", conn);
+                    cmd.Parameters.AddWithValue("@id", dgvPengunjung.SelectedRows[0].Cells["PengunjungID"].Value);
+                    cmd.ExecuteNonQuery();
+                    LoadPengunjung();
+                }
+            }
+        }
+        #endregion
+
+        #region — Kelola Tiket / Pesanan —
         private void LoadPesanan()
         {
-            SqlConnection conn = new SqlConnection(connString);
-            try
+            using (SqlConnection conn = new SqlConnection(connString))
             {
-                SqlDataAdapter da = new SqlDataAdapter(@"
-                    SELECT p.PesananID, q.Nama AS NamaPengunjung, w.NamaWahana, 
-                           p.TanggalKunjungan, p.JumlahTiket, p.TotalHarga, 
-                           p.MetodePembayaran, p.StatusPesanan
-                    FROM Pesanan p
-                    JOIN Pengunjung q ON p.PengunjungID = q.PengunjungID
-                    JOIN Wahana w ON p.WahanaID = w.WahanaID", conn);
+                SqlDataAdapter da = new SqlDataAdapter(@"SELECT p.PesananID, q.Nama AS Pengunjung, w.NamaWahana, p.TanggalKunjungan, p.JumlahTiket, p.TotalHarga, p.StatusPesanan FROM Pesanan p JOIN Pengunjung q ON p.PengunjungID=q.PengunjungID JOIN Wahana w ON p.WahanaID=w.WahanaID", conn);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
                 dgvPesanan.DataSource = dt;
-            }
-            finally
-            {
-                conn.Dispose();
             }
         }
 
         private void btnTambahPesanan_Click(object sender, EventArgs e)
         {
-            SqlConnection conn = new SqlConnection(connString);
-            try
+            using (SqlConnection conn = new SqlConnection(connString))
             {
                 conn.Open();
-                decimal harga = GetHargaWahana((int)cmbWahanaPesanan.SelectedValue);
+                SqlCommand cmd = new SqlCommand("INSERT INTO Pesanan (PengunjungID, WahanaID, TanggalKunjungan, JumlahTiket, TotalHarga, MetodePembayaran, StatusPesanan) VALUES (@pengunjung, @wahana, @tanggal, @jumlah, @total, 'Tunai', 'Dipesan')", conn);
                 int jumlah = int.Parse(txtJumlahTiket.Text);
-                SqlCommand cmd = new SqlCommand(@"
-                    INSERT INTO Pesanan (PengunjungID, WahanaID, TanggalKunjungan, 
-                                         JumlahTiket, TotalHarga, MetodePembayaran, StatusPesanan)
-                    VALUES (@pengunjung, @wahana, @tanggal, @jumlah, @total, @metode, @status)", conn);
+                decimal harga = GetHargaWahana((int)cmbWahanaPesanan.SelectedValue);
+
                 cmd.Parameters.AddWithValue("@pengunjung", (int)cmbPengunjung.SelectedValue);
                 cmd.Parameters.AddWithValue("@wahana", (int)cmbWahanaPesanan.SelectedValue);
                 cmd.Parameters.AddWithValue("@tanggal", dtpTanggal.Value.Date);
                 cmd.Parameters.AddWithValue("@jumlah", jumlah);
                 cmd.Parameters.AddWithValue("@total", harga * jumlah);
-                cmd.Parameters.AddWithValue("@metode", "Cash"); // Contoh metode pembayaran
-                cmd.Parameters.AddWithValue("@status", "Dipesan");
                 cmd.ExecuteNonQuery();
                 LoadPesanan();
-            }
-            finally
-            {
-                conn.Dispose();
             }
         }
 
         private decimal GetHargaWahana(int wahanaID)
         {
-            SqlConnection conn = new SqlConnection(connString);
-            try
+            using (SqlConnection conn = new SqlConnection(connString))
             {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("SELECT HargaTiket FROM Wahana WHERE WahanaID = @id", conn);
                 cmd.Parameters.AddWithValue("@id", wahanaID);
                 return (decimal)cmd.ExecuteScalar();
             }
-            finally
+        }
+
+        private void btnUpdateStatus_Click(object sender, EventArgs e)
+        {
+            if (dgvPesanan.SelectedRows.Count > 0)
             {
-                conn.Dispose();
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE Pesanan SET StatusPesanan=@status WHERE PesananID=@id", conn);
+                    cmd.Parameters.AddWithValue("@status", cmbStatusPesanan.Text);
+                    cmd.Parameters.AddWithValue("@id", dgvPesanan.SelectedRows[0].Cells["PesananID"].Value);
+                    cmd.ExecuteNonQuery();
+                    LoadPesanan();
+                }
+            }
+        }
+
+        private void btnHapusPesanan_Click(object sender, EventArgs e)
+        {
+            if (dgvPesanan.SelectedRows.Count > 0)
+            {
+                using (SqlConnection conn = new SqlConnection(connString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("DELETE FROM Pesanan WHERE PesananID=@id", conn);
+                    cmd.Parameters.AddWithValue("@id", dgvPesanan.SelectedRows[0].Cells["PesananID"].Value);
+                    cmd.ExecuteNonQuery();
+                    LoadPesanan();
+                }
             }
         }
         #endregion
